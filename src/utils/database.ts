3bcc06ff26ab38448ce84ccdc6afa9fe14252628
Joseph P Langford <joseph.p.langford@gmail.com>
2025-05-15 21:42:35 -0400

    Remove SupabaseHandler, create room for self-hosted database

---
+++ src/utils/database.ts
@@ -0,0 +1,18 @@
+// Database: gurubot
+// Table: managedvoicechannels
+// Column: channelId (text)
+// Column: guildId (text)
+// Column: id (uuid)
+
+// Create a query to connect to the database and ensure the table exists
+// Create a query to get, insert, and delete managed voice channels
+// Create a query to get managed voice channels by server
+
+// Table: logchannels
+// Column: channelId (text)
+// Column: guildId (text)
+// Column: id (uuid)
+
+// Create a query to connect to the database and ensure the table exists
+// Create a query to get, insert, and delete log channels
+// Create a global list of log channels to ignore when sending messages
